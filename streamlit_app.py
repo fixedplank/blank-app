b0d279caf4e165cb38f11a6590ed499cf480b700
fixedplank <fixedplank@gmail.com>
2026-05-02 15:28:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,7 @@ from datetime import datetime, timedelta
 # GLOBAL SETUP
 # ─────────────────────────────────────────────────────────────────────────────
 
-RAPIDAPI_KEY = "ee57d87135mshd9fd1d61e9d7af6p10cd23jsn12a7f548c63d
-"
+RAPIDAPI_KEY = "ee57d87135mshd9fd1d61e9d7af6p10cd23jsn12a7f548c63d"
 HEADERS = {
     "x-rapidapi-key": RAPIDAPI_KEY,
     "x-rapidapi-host": "sofascore.p.rapidapi.com",
